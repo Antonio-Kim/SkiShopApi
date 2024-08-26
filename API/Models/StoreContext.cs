5b0a3110b6a3ac9fdee5c6bc62cff478c586ae0f
Antonio Kim <antoniok@sfu.ca>
2024-08-26 16:56:27 -0700

    Setting up checkout entities

--- API/Models/StoreContext.cs
+++ API/Models/StoreContext.cs
@@ -1,10 +1,11 @@
+using API.Models.OrderAggregate;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Models;
 
-public class StoreContext : IdentityDbContext<User>
+public class StoreContext : IdentityDbContext<User, Role, int>
 {
     public StoreContext(DbContextOptions options) : base(options)
     {
@@ -12,15 +13,22 @@ public class StoreContext : IdentityDbContext<User>
 
     public DbSet<Product> Products => Set<Product>();
     public DbSet<Cart> Carts => Set<Cart>();
+    public DbSet<Order> Orders => Set<Order>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
-        builder.Entity<IdentityRole>()
+        builder.Entity<User>()
+            .HasOne(a => a.Address)
+            .WithOne()
+            .HasForeignKey<UserAddress>(a => a.Id)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Role>()
             .HasData(
-                new IdentityRole { Name = "Member", NormalizedName = "MEMBER" },
-                new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" }
+                new Role { Id = 1, Name = "Member", NormalizedName = "MEMBER" },
+                new Role { Id = 2, Name = "Admin", NormalizedName = "ADMIN" }
             );
     }
 }
